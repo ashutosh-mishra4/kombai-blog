84e9432ddf61e735ee1849b54079d5e488492875
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2023-04-05 14:13:55 +0530

    Remove menu, contacts, and copyright

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,9 +3,6 @@ import React from "react";
 import { useSiteMetadata } from "@/hooks";
 
 import { Author } from "./Author";
-import { Contacts } from "./Contacts";
-import { Copyright } from "./Copyright";
-import { Menu } from "./Menu";
 
 import * as styles from "./Sidebar.module.scss";
 
@@ -14,15 +11,12 @@ type Props = {
 };
 
 const Sidebar = ({ isIndex }: Props) => {
-  const { author, copyright, menu } = useSiteMetadata();
+  const { author } = useSiteMetadata();
 
   return (
     <div className={styles.sidebar}>
       <div className={styles.inner}>
         <Author author={author} isIndex={isIndex} />
-        <Menu menu={menu} />
-        <Contacts contacts={author.contacts} />
-        <Copyright copyright={copyright} />
       </div>
     </div>
   );
